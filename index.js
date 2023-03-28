0ed69ec8a9465e0194a2ae1cef7e755f6ff0eaf9
helmet5832 <124384963+helmet5832@users.noreply.github.com>
2023-03-28 20:34:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ async function discordMain() {
         console.log("Failed to loading user information from discord api! try to set user data with default data (FAILBACK)")
         profileStatus.setAttribute("status","none")
         profilePictureImage.setAttribute("src",failbackProfileImage)
-        profileName.textContent = "JIHOO"
+        profileName.textContent = "Helmet"
     }
 
     try {
