78a86fef6370a0ab3b8faf0e43583b1f4919cb45
Jvcky <124384963+jvcky23@users.noreply.github.com>
2024-01-30 06:22:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,6 @@ async function discordMain() {
 
                 // 이름설정
                 profileName.textContent = user.username
-                profileTag.textContent = '#'+user.discriminator
 
                 // 클립보드에 이름 복사
                 if (discordButton) {
